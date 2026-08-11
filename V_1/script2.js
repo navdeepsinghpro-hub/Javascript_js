32d80d6ea8d7f225da41bf18bc45656a0f1ee688
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-11 21:46:14 +0530

    learning obj

---
+++ V_1/script2.js
@@ -0,0 +1,52 @@
+// let student = {
+//     name: "Navdeep",
+//     age: 20,
+//     marks: 85
+// };
+
+// student.name;
+
+// let student = {
+//     name: "Navdeep",
+//     age: 20,
+//     marks: 85
+// };
+
+// student["marks"] = 95;
+
+// let student = {
+//     name: "Navdeep",
+//     age: 20
+// };
+
+// student["city"] = "delhi";
+
+// let student = {
+//     name: "Navdeep",
+//     age: 20,
+//     marks: 85
+// };
+
+// let student = {
+//     name: "Navdeep",
+//     age: 20,
+//     marks: 85
+// };
+
+// let student = {
+//     name: "Navdeep",
+//     age: 20,
+//     marks: 85
+// };
+
+// let student = {
+//     name: "Navdeep",
+//     age: 20,
+//     marks: 85
+// };
+
+let student = {
+    name: "Navdeep",
+    age: 20,
+    marks: 85
+};

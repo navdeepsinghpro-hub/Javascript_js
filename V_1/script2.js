4005761c93d722a22c5f4ae6890d8c13197ef67f
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-12 19:44:57 +0530

    learn objects

--- V_1/script2.js
+++ V_1/script2.js
@@ -45,8 +45,36 @@
 //     marks: 85
 // };
 
+// let student = {
+//     name: "Navdeep",
+//     age: 20,
+//     marks: 85
+// };
+
+// let student = {
+//     name: "Navdeep",
+//     marks: [80, 90, 85, 95]
+// };
+
+// let student = {
+//     name: "Navdeep",
+//     marks: [60, 75, 80, 90, 95]
+// };
+
+// let student = {
+//     name: "Navdeep",
+//     address: {
+//         city: "Delhi",
+//         pincode: 110001
+//     }
+// };
+
+// let student = {
+//     name: "Navdeep",
+//     marks: 85
+// };
+
 let student = {
     name: "Navdeep",
-    age: 20,
-    marks: 85
+    marks: [50, 70, 85, 90, 95]
 };

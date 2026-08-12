ff36f0e5a427c950c48c9c0115c9ac67eb205a18
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-12 21:39:10 +0530

    learning dom

---
+++ V_1/script3.js
@@ -0,0 +1,4 @@
+// let ab = document.querySelector("h1")
+
+let ab = document.querySelectorAll("h1");
+console.dir(ab);
---
+++ V_2/script.js
@@ -0,0 +1,12 @@
+// let ab = document.querySelector("h1")
+
+// let ab = document.querySelectorAll("h1");
+// console.dir(ab);
+
+// let h1 = document.querySelector("h1");
+// h1.innerHTML = "Yo Yo Honet Singh"
+
+// let a= document.querySelector("a");
+// console.dir(a);
+// a.href = "https://www.google.com"
+// a.setAttribute("href", "https://www.google.com");

e8b059a03dc82993ca071d0e343474c69e2d07d1
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-14 20:33:58 +0530

    learning event

---
+++ V_2/script1.js
@@ -0,0 +1,29 @@
+// let inp = document.querySelector("input");
+
+// inp.addEventListener("input", function(val){
+//     console.log(val.data);
+// });
+
+// let sel = document.querySelector("select");
+// let devive = document.querySelector("#device")
+
+// sel.addEventListener("change", function(dets){
+//     device.textContent = "Device Selected";
+// });
+
+// let h1 = document.querySelector("h1");
+
+// window.addEventListener("keydown", function(dets){
+//       h1.textContent = dets.key;
+// });
+
+// let btn = document.querySelector("#btn");
+// let fileinp = document.querySelector("#fileinp");
+
+// btn.addEventListener("click", function(){
+//     fileinp.click();
+// });
+
+// fileinp.addEventListener("change", function(dets){
+//     btn.textContent = dets.target.files[0].name;
+// });

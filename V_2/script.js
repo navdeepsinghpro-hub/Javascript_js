41e50cc6db3eb2b9d024403d488425727acb7c58
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-14 16:49:19 +0530

    dom loaded

--- V_2/script.js
+++ V_2/script.js
@@ -10,3 +10,11 @@
 // console.dir(a);
 // a.href = "https://www.google.com"
 // a.setAttribute("href", "https://www.google.com");
+
+// let h1 = document.createElement("h1");
+// h1.textContent = 'Hello';
+// document.body.prepend(h1);
+
+// let h1 = document.querySe lector("h1");
+// h1.style.color = "red"
+// h1.style.backgroundColor = "skyblue"

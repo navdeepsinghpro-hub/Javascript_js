d84485983d1732aca58524a0bf5601ba2fee7181
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-15 11:53:39 +0530

    Learned events

--- V_2/script1.js
+++ V_2/script1.js
@@ -27,3 +27,38 @@
 // fileinp.addEventListener("change", function(dets){
 //     btn.textContent = dets.target.files[0].name;
 // });
+
+// let div = document.querySelector("div");
+
+// let h1 = document.createElement("h1");
+// let push = h1.textContent = "hello";
+
+// div.append(push);
+
+// let abcd = document.querySelector("#abcd");
+
+// abcd.addEventListener("mouseover", function(){
+//     abcd.style.backgroundColor = "blue";
+// });
+
+// abcd.addEventListener("mouseout", function(){
+//     abcd.style.backgroundColor = "red";
+// });
+
+// window.addEventListener("mousemove", function(dets){
+//     abcd.style.top = dets.clientY +"px";
+//     abcd.style.left = dets.clientX + "px";
+// });
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function(){
+//     alert("OH YES ")
+// })
+
+// let inp = document.querySelector("input");
+// let span = document.querySelector("span")
+
+// inp.addEventListener("input", function(){
+//     span.textContent = inp.value.length;
+// });

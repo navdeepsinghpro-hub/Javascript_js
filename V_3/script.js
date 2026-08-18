ea9e1d80f3c81bc03dd77a55252203499c354220
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-18 21:35:18 +0530

    learning js

--- V_3/script.js
+++ V_3/script.js
@@ -1,10 +1,51 @@
-let obj = {
-    name: "navdeep",
-    age: 20
-};
+// let obj = {
+//     name: "navdeep",
+//     age: 20
+// };
 
-function ab () {
-    console.log(this.age);
-};
+// function ab () {
+//     console.log(this.age);
+// };
 
-ab.call(obj);
+// ab.call(obj);
+
+// function ab(name) {
+//     this.name = name;
+// };
+
+// let cd = new ab("navdeep");
+
+// function pencil(name, color, price, ){
+//     this.name = name;
+//     this.color = color;
+//     this.price = price;
+// };
+
+// pencil.prototype.company = "Navdeep"
+
+// let p1 = new pencil("natraj", "red", 5);
+
+class Pencil {
+    constructor(name,company,color,price){
+        this.name = name;
+        this.company = company;
+        this.color = color;
+        this.price = price;
+    }
+
+    write(text){
+        let h1 = document.createElement("h1");
+        h1.textContent = text;
+        h1.style.color = this.color;
+        document.body.appendChild(h1);
+    }
+
+    erase(){
+        do {
+            
+        } while (condition);
+    }
+}
+
+let p1 = new Pencil("natraj", "natraj", "black", 10);
+let p2 = new Pencil("apsra", "apsra", "red", 15);

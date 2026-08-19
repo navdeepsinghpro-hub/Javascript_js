3b51421207307084dc0477d503c6e03f3071bd1c
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-19 21:30:40 +0530

    learn async

--- V_3/script.js
+++ V_3/script.js
@@ -1,51 +1,76 @@
-// let obj = {
-//     name: "navdeep",
-//     age: 20
-// };
+// // let obj = {
+// //     name: "navdeep",
+// //     age: 20
+// // };
 
-// function ab () {
-//     console.log(this.age);
-// };
+// // function ab () {
+// //     console.log(this.age);
+// // };
 
-// ab.call(obj);
+// // ab.call(obj);
 
-// function ab(name) {
-//     this.name = name;
-// };
+// // function ab(name) {
+// //     this.name = name;
+// // };
 
-// let cd = new ab("navdeep");
+// // let cd = new ab("navdeep");
 
-// function pencil(name, color, price, ){
-//     this.name = name;
-//     this.color = color;
-//     this.price = price;
-// };
+// // function pencil(name, color, price, ){
+// //     this.name = name;
+// //     this.color = color;
+// //     this.price = price;
+// // };
 
-// pencil.prototype.company = "Navdeep"
+// // pencil.prototype.company = "Navdeep"
 
-// let p1 = new pencil("natraj", "red", 5);
+// // let p1 = new pencil("natraj", "red", 5);
 
-class Pencil {
-    constructor(name,company,color,price){
-        this.name = name;
-        this.company = company;
-        this.color = color;
-        this.price = price;
-    }
+// class Pencil {
+//     constructor(name,company,color,price){
+//         this.name = name;
+//         this.company = company;
+//         this.color = color;
+//         this.price = price;
+//     }
 
-    write(text){
-        let h1 = document.createElement("h1");
-        h1.textContent = text;
-        h1.style.color = this.color;
-        document.body.appendChild(h1);
-    }
+//     write(text){
+//         let h1 = document.createElement("h1");
+//         h1.textContent = text;
+//         h1.style.color = this.color;
+//         document.body.appendChild(h1);
+//     }
 
-    erase(){
-        do {
+//     erase(){
+//         do {
             
-        } while (condition);
-    }
-}
+//         } while (condition);
+//     }
+// }
 
-let p1 = new Pencil("natraj", "natraj", "black", 10);
-let p2 = new Pencil("apsra", "apsra", "red", 15);
+// let p1 = new Pencil("natraj", "natraj", "black", 10);
+// let p2 = new Pencil("apsra", "apsra", "red", 15);
+
+// let pr = new Promise(function(res, rej){
+//     setTimeout(() => {
+//         let rn = Math.floor(Math.random()*5);
+//         if(rn>5) res("resolved with " + rn)
+//         else rej("rejected with " + rn);
+//     }, 1000);
+// });
+
+// pr.then(function(val){
+//     console.log(val);
+// }).catch(function(val){
+//     console.log(val);
+// });
+
+// async function abcd(){
+//     try{
+//         let val = await pr;
+//     console.log(val);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// abcd();

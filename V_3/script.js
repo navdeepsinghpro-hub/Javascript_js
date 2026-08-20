f5d449b64e638a7fa4c020923497a446913bd6bc
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-20 20:19:38 +0530

    learning js

--- V_3/script.js
+++ V_3/script.js
@@ -74,3 +74,18 @@
 // }
 
 // abcd();
+
+// fetch("https://randomuser.me/api/")
+// .then((rawdata) => {
+//     return rawdata.json;
+// })
+// .then ((data) => {
+//     console.log(data.results);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+
+fetch("https://randomuser.me/api/?results=5")
+    .then(raw => raw.json())
+    .then(data => console.log(data.results[0].name.first));

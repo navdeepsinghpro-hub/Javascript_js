e8f716ebadaffe65459e619970fc452c8c03e9e0
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-21 21:36:46 +0530

    learn  js

--- V_4/script.js
+++ V_4/script.js
@@ -8,19 +8,30 @@
 //     }
 // }
 
-function throttle(fnc, delay){
-    let timer =0;
-    return function(...args){
-        let now = Date.now();
-        if(now - timer >= delay){
-            timer = now;
-            fnc(...args);
-        }
-    };
-}
+// function throttle(fnc, delay){
+//     let timer =0;
+//     return function(...args){
+//         let now = Date.now();
+//         if(now - timer >= delay){
+//             timer = now;
+//             fnc(...args);
+//         }
+//     };
+// }
 
-let inp = document.querySelector("input");
-inp.addEventListener("input", throttle(function(){
-    console.log("hey");
-}, 1000));
+// let inp = document.querySelector("input");
+// inp.addEventListener("input", throttle(function(){
+//     console.log("hey");
+// }, 1000));
 
+const btn = document.querySelector("button")
+const ul = document.querySelector("ul")
+
+btn.addEventListener("click", function(){
+    const num1 = Math.floor(Math.randon()*10);
+    const num2 = Math.floor(Math.randon()*10);
+    let add = num1+num2;
+    let li = document.createElement("li")
+    li.textContent = add;
+    ul.append(li);
+});
